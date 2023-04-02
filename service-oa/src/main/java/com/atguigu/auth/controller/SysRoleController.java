6c67bb25b2132da154c148d9ad1d1ea5beeaaab8
JarvisTH <1373661072@qq.com>
2023-04-02 18:31:35 +0800

    implement:003 reason:sysRole的其他接口实现

--- service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
@@ -3,13 +3,18 @@ package com.atguigu.auth.controller;
 import com.atguigu.auth.service.SysRoleService;
 import com.atguigu.common.result.Result;
 import com.atguigu.model.system.SysRole;
+import com.atguigu.vo.system.SysRoleQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.mysql.cj.util.StringUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "角色管理接口")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -17,9 +22,82 @@ public class SysRoleController {
     @Autowired
     private SysRoleService sysRoleService;
 
+    @ApiOperation("查询所有角色")
     @GetMapping("findAll")
     public Result findAll() {
         List<SysRole> list = sysRoleService.list();
         return Result.ok(list);
+    }
+
+    // page 当前页  limit 每页显示的记录数 sysRoleQueryVo 条件对象
+    @ApiOperation("条件分页查询")
+    @GetMapping("{page}/{limit}")
+    public Result pageQueryRole(@PathVariable Long page,
+                                @PathVariable Long limit,
+                                SysRoleQueryVo sysRoleQueryVo) {
+        // 1.创建一个page对象，传递分页参数
+        Page<SysRole> rolePage = new Page<>(page, limit);
+
+        // 2.封装条件，判断条件值是否为空，不为空则封装
+        LambdaQueryWrapper<SysRole> wrapper = new LambdaQueryWrapper<>();
+        String roleName = sysRoleQueryVo.getRoleName();
+        if (!StringUtils.isEmptyOrWhitespaceOnly(roleName)) {
+            wrapper.like(SysRole::getRoleName, roleName);
+        }
+
+        // 3.进行条件分页查询
+        Page<SysRole> sysRolePage = sysRoleService.page(rolePage, wrapper);
+        return Result.ok(sysRolePage);
+    }
+
+    @ApiOperation("添加角色")
+    @PostMapping("save")
+    public Result save(@RequestBody SysRole sysRole) {
+        boolean isSave = sysRoleService.save(sysRole);
+        if (isSave) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("根据id查询")
+    @GetMapping("get/{id}")
+    public Result get(@PathVariable Long id) {
+        SysRole sysRole = sysRoleService.getById(id);
+        return Result.ok(sysRole);
+    }
+
+    @ApiOperation("修改角色")
+    @PutMapping("update")
+    public Result update(@RequestBody SysRole sysRole) {
+        boolean isUpdate = sysRoleService.updateById(sysRole);
+        if (isUpdate) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("根据id删除角色")
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        boolean isRemove = sysRoleService.removeById(id);
+        if (isRemove) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("批量删除角色")
+    @DeleteMapping("batchRemove")
+    public Result batchRemove(@RequestBody List<Long> idList) {
+        boolean isRemove = sysRoleService.removeByIds(idList);
+        if (isRemove) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
     }
 }
